d26f8c006ef142e0e87bc2d2b449b0f647120f37
obakiner <onur.bakiner@gmail.com>
2021-02-07 10:29:22 -0800

    Added RollDice_Invalid_Roll_0_Dice_0_Should_Return_Zero to DiceHelperUnitTest.cs

--- NUnitTestProject1/Helpers/DiceHelperUnitTest.cs
+++ NUnitTestProject1/Helpers/DiceHelperUnitTest.cs
@@ -36,6 +36,20 @@ namespace UnitTests.Helpers
         }
 
         [Test]
+        public void RollDice_Invalid_Roll_0_Dice_0_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(0, 0);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
         public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_And_6()
         {
             // Arrange
