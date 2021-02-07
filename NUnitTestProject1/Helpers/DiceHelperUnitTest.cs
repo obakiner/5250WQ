4b578bf14e67499716ba882c71ea1e076bd87689
obakiner <onur.bakiner@gmail.com>
2021-02-07 10:26:54 -0800

    RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_2_And_12 added to DiceHelperUnitTest.cs

--- NUnitTestProject1/Helpers/DiceHelperUnitTest.cs
+++ NUnitTestProject1/Helpers/DiceHelperUnitTest.cs
@@ -37,6 +37,23 @@ namespace UnitTests.Helpers
         }
 
         [Test]
+        public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_2_And_12()
+        {
+            // Arrange
+
+            // Act
+            var result1 = DiceHelper.RollDice(1, 6);
+            var result2 = DiceHelper.RollDice(1, 6);
+            var result = result1 + result2; 
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, result >= 2);
+            Assert.AreEqual(true, result <= 12);
+        }
+
+        [Test]
         public void RollDice_Invalid_Roll_Forced_1_Should_Return_1()
         {
             // Arrange 
