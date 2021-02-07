5d741cda340f9c7564406c07122a50d11564a3ab
obakiner <onur.bakiner@gmail.com>
2021-02-07 08:59:06 -0800

    Added ItemModel_Get_Valid_Default_Should_Pass to ItemModelTests.cs

--- NUnitTestProject1/Models/ItemModelTests.cs
+++ NUnitTestProject1/Models/ItemModelTests.cs
@@ -41,6 +41,19 @@ namespace UnitTests.Models
             Assert.AreEqual("Text", result.Text);
             Assert.AreEqual(1, result.Value);
         }
+
+        public void ItemModel_Get_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result.Value);
+        }
     }
 }
 
